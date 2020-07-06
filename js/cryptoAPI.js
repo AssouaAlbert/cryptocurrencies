ab949d6127e76ba9a28f69ef4349c48bb3cd1acd
Eloumbat Assoua Albert <assouaalbert@gmail.com>
2020-07-06 10:22:31 +0300

    API is deprecated

--- js/cryptoAPI.js
+++ js/cryptoAPI.js
@@ -15,12 +15,16 @@ class CryptoAPI{
         }
     }
     async getCrytoCurrencies(){
-        const url = await this.request('GET', "https://api.coinmarketcap.com/v1/ticker/");
+        const url = await this.request("https://api.coinmarketcap.com/v1/ticker/");
         // const url = await this.request('GET', "https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY=53d95d9b-6074-4713-b323-8592222241e4");
-        console.log('url: ', url.target.responseText);
         return JSON.parse(url.target.responseText);
         // const cryptoCurrencies = await JSON.parse(url.responseText);
     }
+
+    // async request (url) {
+    //     const response = await fetch(url).then(response =>  response.json());
+
+    // }
 
     request(method, url){
         return new Promise((resolve,reject)=>{;
